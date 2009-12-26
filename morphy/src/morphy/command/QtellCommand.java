6fc3e3dd31155fc303559d4d3cf603b43556aa3e
John.Nahlen <John.Nahlen@4c9440f2-ede6-11de-9298-fbd6a473cdd1>
2009-12-26 02:31:15 +0000

    Some commands, help files, utilities

---
+++ morphy/src/morphy/command/QtellCommand.java
@@ -0,0 +1,35 @@
+package morphy.command;
+
+import morphy.service.UserService;
+import morphy.user.UserLevel;
+import morphy.user.UserSession;
+
+public class QtellCommand extends AbstractCommand {
+
+	public QtellCommand() {
+		super("qtell");
+	}
+
+	public void process(String arguments, UserSession userSession) {
+//		if (userSession.getUser().getUserLevel() != UserLevel.Bot) { 
+//			userSession.send("Only TD programs and admins are allowed to use this command.");
+//			return;
+//		}
+		
+		int whiteSpacePos = arguments.indexOf(" ");
+		if (whiteSpacePos == -1) {
+			userSession.send(getContext().getUsage());
+			return;
+		}
+		String userName = arguments.substring(0,whiteSpacePos);
+		String message = arguments.substring(whiteSpacePos+1,arguments.length()-1);
+		
+		UserSession sendTo = UserService.getInstance().getUserSession(userName);
+		if (sendTo != null) {
+			sendTo.send(":" + message);
+		}
+		
+		userSession.send("*qtell " + userName + " " + ((sendTo.isConnected())?"1":"0") );
+	}
+	
+}
